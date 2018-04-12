d94b17cafdbc2adef3ae5c426535088d43ad0af4
Daniel <daniel.smith@pitchbook.com>
2018-04-12 12:56:58 -0700

    update some comments and import statement

--- alpha_vantage/alpha_vantage.py
+++ alpha_vantage/alpha_vantage.py
@@ -1,5 +1,5 @@
 import json
-import urllib
+import urllib.request
 
 
 class AlphaVantage:
@@ -24,16 +24,36 @@ class AlphaVantage:
         key = info[1].split("=")[1].strip()
         return user, key
 
-    def daily_returns(self, ticker, output='compact'):
+    def send_request(self, func, ticker, interval=None, output='compact'):
         """
         Get the daily returns associated with a ticker argument.
 
+        :param func: Function name to execute. One of:
+            TIME_SERIES_INTRADAY
+            TIME_SERIES_DAILY
+            TIME_SERIES_DAILY_ADJUSTED
+            TIME_SERIES_WEEKLY
+            TIME_SERIES_WEEKLY_ADJUSTED
+            TIME_SERIES_MONTHLY
+            TIME_SERIES_MONTHLY_ADJUSTED
+            
         :param ticker: A string argument representing a company's ticker (e.g. NVDA, IBM, CVX)
-        :param output: The output type that AlphaVantage should return. Defaults to compact, other values include...
+        :param interval: The rate at which to fetch prices. Defaults to None. Can be on of values
+            1min
+            5min
+            15min
+            30min
+            60min
+        :param output: The output type that AlphaVantage should return. Defaults to compact, possible values include
+            compact
+            full
         :return json_response: Return a json object with the daily return data.
         """
-        base_url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={}&outputsize={}&apikey={}"
-        formatted_url = base_url.format(ticker, output, self.key)
+        base_url = "https://www.alphavantage.co/query?function={}&symbol={}&outputsize={}{}&apikey={}"
+        if interval is None:
+            formatted_url = base_url.format(func, ticker, output, '', self.key)
+        else:
+            formatted_url = base_url.format(func, ticker, output, '&interval=' + interval, self.key)
 
         with urllib.request.urlopen(formatted_url) as response:
             json_response = json.loads(response.read())
@@ -45,6 +65,6 @@ if __name__ == '__main__':
     av = AlphaVantage()
     print("user: {}".format(av.user))
     print("API key: {}".format(av.key))
-    result = av.daily_returns("NVDA")
+    result = av.send_request("TIME_SERIES_DAILY", "NVDA")
     pp = pprint.PrettyPrinter(depth=4)
     pp.pprint(result)
