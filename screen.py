bfcecc51200feb079cd85b126af6b6efc443ab04
Daniel <daniel.smith@pitchbook.com>
2019-01-21 20:45:25 -0800

    a few metrics updates

--- screen.py
+++ screen.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 
-import datetime
 import re
-import requests
 import sys
 
 from alpha_vantage.alpha_vantage import AlphaVantage
@@ -64,7 +62,8 @@ class Stock:
         try:
             price = float(self.returns['Time Series (Daily)'][self.today]['4. close'])
         except KeyError:
-            last_business_day = utils.get_most_recent_date(list(self.returns['Time Series (Daily)'].keys()))
+            # last_business_day = utils.get_most_recent_date(list(self.returns['Time Series (Daily)'].keys()))
+            last_business_day = list(self.returns['Time Series (Daily)'].keys())[0]
             price = float(self.returns['Time Series (Daily)'][last_business_day]['4. close'])
 
         return round(price, 2)
@@ -107,10 +106,6 @@ class Stock:
         :return:
         """
         index = str(self.frames[0].columns[-1].year)
-
-        # dividend_payment = data[index]['Dividends USD']
-        # earnings_per_share = data[index]['Earnings Per Share USD']
-        # payout_ratio = round((dividend_payment / earnings_per_share) * 100, 2)
         payout_ratio = self.frames[0][index]['Payout Ratio % *']
         return payout_ratio
 
@@ -152,7 +147,7 @@ class Stock:
         index = str(self.frames[8].columns[-1].year)
         equity = self.frames[8][index]["Total Stockholders' Equity"]
         liabilities = self.frames[8][index]['Total Liabilities']
-        debt_to_equity_ratio = round((liabilities / equity) * 100, 2)
+        debt_to_equity_ratio = round(liabilities / equity, 2)
         return debt_to_equity_ratio
 
     def _get_star_rating(self):
@@ -162,152 +157,17 @@ class Stock:
         """
         star = "\N{BLACK STAR}"
         rating = ""
-        if self.continuous_dividend_increases is True:
-            rating = rating + star
-        if 2.0 <= self.dividend_yield <= 8.0:
-            rating = rating + star
-        if self.dividend_compound_annual_growth_rate >= 6.0:
-            rating = rating + star
-        if self.price_to_earnings_ratio <= 16.0:
-            rating = rating + star
-        if self.dividend_payout_ratio <= 60.0:
-            rating = rating + star
-        if self.debt_to_equity <= 60.0:
-            rating = rating + star
-        if self.discount >= 10.0:
-            rating = rating + star
+        conditions = [self.continuous_dividend_increases is True,
+                      2.0 <= self.dividend_yield <= 8.0,
+                      self.dividend_compound_annual_growth_rate >= 6.0,
+                      self.price_to_earnings_ratio <= 16.0,
+                      self.dividend_payout_ratio <= 60.0,
+                      self.debt_to_equity <= 0.6,
+                      self.discount >= 10.0]
+        for condition in conditions:
+            if condition:
+                rating = rating + star
         return rating
-
-def get_statistics(ticker_symbol, time_periods=5):
-    """
-    @desc   - compile information about a stock from an online source.
-              the stock data gathered can then be used to calculate various
-              financial ratios
-    @param  - ticker_symbol: the stock's ticker code
-    @param  - time_periods: number of years to gather return data for
-    @return - stock: a Stock class object containing many statistics
-    """
-    # make a soup object from yahoo finance page
-    ticker_symbol = ticker_symbol.upper().strip()
-    stock = Stock(ticker_symbol)
-    url = "http://finance.yahoo.com/quote/{}".format(ticker_symbol)
-    soup = make_soup(url)
-
-    # get base urls with links to other information type pages
-    urls = make_base_urls(soup, url)
-    stock.urls = urls
-    try:
-        profile = make_soup(stock.urls["profile"])
-        statistics = make_soup(stock.urls["key-statistics"])
-    except KeyError:
-        return stock
-    # print(re.findall(".{100}1\.96", soup.get_text()))
-    # you will get historical data here
-
-    # gather other stats from this pre-created form
-    params = {"formatted": "true",
-              "crumb": "AKV/cl0TOgz",
-              "lang": "en-US",
-              "region": "US",
-              "modules": "defaultKeyStatistics,financialData,calendarEvents",
-              "corsDomain": "finance.yahoo.com"}
-    url = "https://query1.finance.yahoo.com/v10/finance/quoteSummary/{}"
-    r = requests.get(url.format(ticker_symbol), params=params)
-    calendar_events = r.json()["quoteSummary"]["result"][0]["calendarEvents"]
-    default_key_statistics = r.json()["quoteSummary"]["result"][0]["defaultKeyStatistics"]
-    financial_data = r.json()["quoteSummary"]["result"][0]["financialData"]
-
-    stock.current_price = financial_data["currentPrice"]["raw"]
-    try:
-        book_value = default_key_statistics["bookValue"]["raw"]
-        trailing_eps = default_key_statistics["trailingEps"]["raw"]
-        graham = calc_graham_num(trailing_eps, book_value)
-        stock.discount = (graham / stock.current_price) - 1
-    except KeyError:
-        stock.discount = "N/A"
-    # stock.price_to_book = default_key_statistics["priceToBook"]["raw"] # missing 'raw' field
-    # stock.beta = default_key_statistics["beta"]["raw"] # missing 'raw' field
-    # stock.forward_eps = default_key_statistics["forwardEps"]["raw"]
-
-    # stock.peg_ratio = default_key_statistics["pegRatio"]["raw"] # missing 'raw' field
-    if "raw" in financial_data["debtToEquity"]:
-        stock.debt_to_equity = financial_data["debtToEquity"]["raw"]
-    elif financial_data["debtToEquity"] == {}:
-        stock.debt_to_equity = "N/A"
-    else:
-        stock.debt_to_equity = financial_data["debtToEquity"]
-        print(stock.debt_to_equity)
-        sys.exit(1)
-    # stock.free_cash_flow = financial_data["freeCashflow"]["raw"]
-    # stock.operating_cash_flow = financial_data["operatingCashflow"]["raw"]
-    try:
-        stock.annual_dividend = float(re.findall('"dividendRate":{"raw":(.*?),', soup.get_text())[0])
-        stock.dividend_yield = float(stock.annual_dividend / stock.current_price)
-        stock.dividend_payout_ratio = float(re.findall('payoutRatio".*?(\d*\.?\d+)',  statistics.get_text())[0])
-    except IndexError:
-        stock.annual_dividend = 0.0
-        stock.dividend_yield = 0.0
-        stock.dividend_payout_ratio = 0.0
-    stock.price_to_earnings = float(re.findall("PE Ratio.*?TTM.*?(\d+\.\d+)", soup.get_text())[0])
-    stock.compound_annual_growth_rate = compound_annual_growth_rate(stock.ticker, time_periods)
-    stock.dividend_history = get_dividend_history(stock.ticker, time_periods)
-    stock.periods = time_periods
-    stock.name = re.findall("See the company profile for (.*?)\(%s\)" % ticker_symbol, profile.get_text())[0].strip()
-    stock.name = format_company_name(stock.name)
-    stock.continuous_dividend_growth = continuous_dividend_increases(stock)
-    stock.dividend_cagr_rates = calc_median_dividend_cagr(stock)
-    stock.rating = ""
-    if stock.continuous_dividend_growth is True:
-        stock.rating += "*"
-    if 0.08 > stock.dividend_yield > 0.02:
-        stock.rating += "*"
-    if stock.dividend_cagr_rates >= 0.06:
-        stock.rating += "*"
-    if stock.price_to_earnings <= 16:
-        stock.rating += "*"
-    if stock.dividend_payout_ratio <= 0.6:
-        stock.rating += "*"
-    if stock.debt_to_equity == "N/A":
-        pass
-    elif stock.debt_to_equity <= 60.0:
-        stock.rating += "*"
-    if stock.discount == "N/A":
-        pass
-    elif stock.discount >= 0.1:
-        stock.rating += "*"
-    return stock
-
-
-def get_dividend_history(ticker, n):
-    """
-    @desc   - get historical dividend payout information
-    @param  - ticker: the stock whose data will be gathered
-    @param  - n: an integer representing the number of years to gather data for
-    @return - returns: list of dividend returns
-    """
-    now = datetime.today() - timedelta(days=1)
-    d1 = (now.year-n, now.month, now.day)
-    d2 = (now.year, now.month, now.day)
-    file_handler = fetch_historical_yahoo(ticker, d1, d2, dividends=True)
-    yields = [(line.strip().split(",")[0], float(line.strip().split(",")[1])) for line in file_handler.readlines()[1:]]
-    return yields
-
-
-def get_historical_price_data(ticker, n):
-    """
-    @desc   - get historical high, low and close prices for a stock in a window of years.
-              End date to gather data is always today.
-    @param  - ticker: the stock whose data will be gathered
-    @param  - n: an integer representing the number of years to gather data for
-    @return - returns: list of daily returns in the format
-
-                  [date, open, high, low, close, adj_close]
-    """
-    now = datetime.today() - timedelta(days=1)
-    d1 = (now.year-n, now.month, now.day)
-    d2 = (now.year, now.month, now.day)
-    returns = quotes_historical_yahoo_ohlc(ticker.upper(), d1, d2)
-    return returns
 
 
 def format_company_name(name):
@@ -317,16 +177,6 @@ def format_company_name(name):
     if "Commo" in name:
         name = re.sub("\s*Commo.+", "", name)
     return name
-
-
-def compound_annual_growth_rate(ticker, n=5):
-    result = get_historical_price_data(ticker, n)
-    then_close = result[0][4]
-    today_close = result[-1][4]
-    comp_annual_growth_rate = ((today_close / then_close) ** (1/n)) - 1
-    return comp_annual_growth_rate
-
-
 
 
 def print_summary(stock):
@@ -340,46 +190,13 @@ def print_summary(stock):
                                                                                                  stock.dividend_compound_annual_growth_rate))
     print("    Price to Earnings ratio is less than 16: {:.2f}".format(stock.price_to_earnings_ratio))
     print("    Ratio of dividends to earnings per share is less than 60%: {:.2f}%".format(stock.dividend_payout_ratio))
-    print("    Debt to equity ratio is less than 60%: {:.2f}%".format(stock.debt_to_equity))
+    print("    Debt to equity ratio is less than 0.60: {:.2f}".format(stock.debt_to_equity))
     print("    Price discount is at least 10% of fair value estimate: {:.2f}%".format(stock.discount))
     print("    Star Rating: {}".format(stock.star_rating))
     print("%" * 80)
-
 
 
 if __name__ == "__main__":
     ticker = sys.argv[1].upper().strip()
     this_stock = Stock(ticker)
     print_summary(this_stock)
-    '''
-    periods = int(sys.argv[2])
-    if ticker == "DIVGROW" or ticker == "WINNERS":
-        # with open("watch_list.txt").readlines() as watch_list:
-        if ticker == "DIVGROW":
-            watch_list = open("watch_list.txt", "rb").readlines()
-        else:
-            watch_list = open("winners", "rb").readlines()
-        summed_results = []
-        for line in watch_list:
-            tickers = [x.decode("utf-8") for x in line.strip().split()]
-            results = []
-            for ticker in tickers:
-                try:
-                    result = get_statistics(ticker, periods)
-                    results.append(result)
-                except:
-                    print("Issue parsing {}".format(ticker))
-                    pass
-            # results.sort(key=lambda r: (r.rating, r.dividend_yield))
-            summed_results += results
-        summed_results.sort(key=lambda r: (r.rating, r.dividend_yield))
-        for result in reversed(summed_results):
-            print_summary(result)
-    else:
-        try:
-            result = get_statistics(ticker, periods)
-            print_summary(result)
-        except:
-            print("Issue parsing {}".format(ticker), file=sys.stderr)
-            sys.exit(0)
-'''
